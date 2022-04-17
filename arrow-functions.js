044c4c9a7f946edb24192dc1fb3537d3c0937592
maxhirtens <max@maxhirtenstein.com>
2022-04-16 17:26:09 -0700

    finished arrow funcs

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,3 +6,16 @@
 
 // 1.
 const double = arr => arr.map(val => val * 2);
+
+// function squareAndFindEvens(numbers){
+//   var squares = numbers.map(function(num){
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+// 2.
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
